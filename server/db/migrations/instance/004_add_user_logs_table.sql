b376ea3cc433eea69ca13d5308d0da9a72fcd47f
Nicholas Willmott <nicholaswillmottse@gmail.com>
2026-01-19 14:27:25 +0800

    Deploy version 1.6.40 at 2026_01_19_14_27_25

---
+++ server/db/migrations/instance/004_add_user_logs_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE user_logs (
+  id SERIAL PRIMARY KEY,
+  user_email text NOT NULL,
+  timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  endpoint text NOT NULL,
+  endpoint_result text NOT NULL,
+  FOREIGN KEY (user_email) REFERENCES users(email)
+);
