50e7196f4593f1308a1c9a1baa797a5db424f4c0
timroberton <timroberton@gmail.com>
2026-09-23 14:27:53 +0800

    panther sync 1 files at 2026-09-23T06:27:53.454Z

--- panther/_303_components/tables/data_grid/data_grid.tsx
+++ panther/_303_components/tables/data_grid/data_grid.tsx
@@ -130,7 +130,7 @@ export function DataGrid(p: DataGridProps) {
       class="w-fit max-w-full max-h-full overflow-auto rounded border"
       style={{ "max-height": p.maxHeight }}
     >
-      <table class="ui-text-small border-separate border-spacing-0">
+      <table class="ui-text-small border-separate border-spacing-0 [&_tr>*:last-child]:border-r-0 [&_tbody>tr:last-child>*]:border-b-0">
         <thead class="bg-base-200 sticky top-0 z-20">
           <Show when={groupSpans()} keyed>
             {(spans) => (
